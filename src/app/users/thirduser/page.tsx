af76af7b28b0d9dc26cf230cfa4e675529c9bedc
joeyparagas <joey.paragas@gmail.com>
2024-05-06 23:17:38 -0700

    Layouts

---
+++ src/app/users/thirduser/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function page() {
+    return (
+        <div className="text-2xl">
+            <h1>Third User</h1>
+            <p>This uses a diffrent layout.tsx file</p>
+        </div>
+    );
+}
